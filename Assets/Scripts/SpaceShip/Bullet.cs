e993882561997c4c8b99685247d4c5fa73a1b81b
Shirly Ruiz 🎮✨ <shirlyruizmendoza6a@gmail.com>
2026-04-17 17:49:56 -0500

    JAKSJAKS

--- Assets/Scripts/SpaceShip/Bullet.cs
+++ Assets/Scripts/SpaceShip/Bullet.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float speed = .1f;
-    [SerializeField] private float killDistance = 1000f;
-
+    [SerializeField] private float killDistance = 1000f; 
     private void Update()
     {
         transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
         if (transform.position.y > killDistance)
         {
             Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject); // mata al enemigo
+            Destroy(gameObject);       // destruye la bala
         }
     }
 }
